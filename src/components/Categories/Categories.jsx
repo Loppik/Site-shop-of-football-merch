4fe2e28a288c595cca149236dc88867892289ae6
Loppik <shantyr_qq@mail.ru>
2018-11-13 22:46:28 +0300

    redesigned Categories component

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import '../../styles/categories.css';
+import './categories.css';
 
 class Categories extends Component {
   constructor(props) {
