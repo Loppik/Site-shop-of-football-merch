eb5ad51cf7ba50c195a0d4772f6869f20d5844e3
Loppik <shantyr_qq@mail.ru>
2018-11-28 11:50:20 +0300

    changed AddShoesForm component

--- src/components/AddShoesForm/AddShoesForm.jsx
+++ src/components/AddShoesForm/AddShoesForm.jsx
@@ -2,58 +2,121 @@ import React, { Component } from 'react';
 import Input from '../UI/Input/Input';
 import Button from '../UI/Button/Button';
 import DropDownMenu from '../UI/DropDownMenu/DropDownMenu';
+import axios from '../../axios';
 
 import './addShoesForm.css';
 
 class AddShoesForm extends Component {
   state = {
-    inputNameData: {
-      labelName: 'Name',
-      type: 'text',
-      name: 'name',
-    },
-    inputDescriptionData: {
-      labelName: 'Description',
-      type: 'text',
-      name: 'description',
-    },
-    inputPriceData: {
-      labelName: 'Price',
-      type: 'text',
-      name: 'price',
-    },
-    buttonLoadData: {
-      text: 'Load',
-    },
-    buttonAddData: {
-      text: 'Add',
-    },
-    selectMenuData: {
-      options: [
-        {
-          text: 'opt1',
-        },
-        {
-          text: 'opt2',
-        },
-      ],
-    },
+    name: '',
+    description: '',
+    price: null,
+    type: null,
+    image: null,
+    categories: null,
+  }
+
+  componentDidMount() {
+    axios.get('categories').then((response) => {
+      const { categories } = response.data;
+      this.setState({ categories });
+      this.setState({ type: categories[0].name })
+    });
+  }
+
+  onChangeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  }
+
+  onChangeDescription = (event) => {
+    this.setState({
+      description: event.target.value,
+    });
+  }
+
+  onChangePrice = (event) => {
+    this.setState({
+      price: event.target.value,
+    });
+  }
+
+  onChangeType = (event) => {
+    this.setState({
+      type: event.target.options[event.target.selectedIndex].text,
+    });
+  }
+
+  onLoadImage = (event) => {
+
+  }
+
+  onAddShoes = (event) => {
+    const { name, description, price, type } = this.state;
+    const shoes = {
+      name,
+      description,
+      type,
+      price,
+    }
+    console.log(shoes)
+    axios.post('shoes', shoes).then((response) => {
+      if (response.status == 200) {
+        alert('Я программист')
+      }
+    }, (err) => {
+      console.log(err)
+    })
   }
 
   render() {
+    const { categories } = this.state;
     return (
       <div className="addShoesForm">
-        <Input data={this.state.inputNameData} />
-        <DropDownMenu data={this.state.selectMenuData}/>
-        <Input data={this.state.inputDescriptionData} />
-        <Input data={this.state.inputPriceData} />
-        <div className="bottom-buttons">
+        { categories && (
           <div>
-            <p>Image:</p>
-            <Button data={this.state.buttonLoadData} /> 
+            <Input
+              value={this.name}
+              label="Name"
+              type="text"
+              name="name"
+              onChange={this.onChangeName}
+            />
+            <DropDownMenu
+              options={categories}
+              onChange={this.onChangeType}
+            />
+            <Input
+              value={this.description}
+              label="Description"
+              type="text"
+              name="description"
+              onChange={this.onChangeDescription}
+            />
+            <Input
+              value={this.price}
+              label="Price"
+              type="text"
+              name="price"
+              onChange={this.onChangePrice}
+            />
+            <div className="bottom-buttons">
+              <div>
+                <p>Image:</p>
+                <Button
+                  text="Load"
+                  onClick={this.onLoadImage}
+                />
+              </div>
+              <Button
+                text="Add"
+                onClick={this.onAddShoes}
+              />
+            </div>
           </div>
-          <Button data={this.state.buttonAddData} />
-        </div>
+        )
+        }
       </div>
     );
   }
