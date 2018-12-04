c2793841498137afe12a37a18270f92c96fddb6d
Loppik <shantyr_qq@mail.ru>
2018-12-04 20:20:51 +0300

    added wishes validation criteria

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -3,44 +3,43 @@ const isInvalidLogin = (login) => {
     return 'incorrect login length';
   }
   const regexp = new RegExp('^[a-zA-Z0-9_]*$');
-  if (login.search(regexp) == -1) {
+  if (login.search(regexp) === -1) {
     return 'incorrect symbol in login';
   }
   return false;
-}
+};
 
 const isInvalidPassword = (password) => {
   if (password.length < 3 || password.length > 15) {
     return 'incorrect password length';
   }
   const regexp = new RegExp('^[a-zA-Z0-9_]*$');
-  if (password.search(regexp) == -1) {
+  if (password.search(regexp) === -1) {
     return 'incorrect symbol in password';
   }
   return false;
-}
+};
 
 const isInvalidPhoneNumber = (phoneNumber) => {
-  if (phoneNumber.toString().length != 7) {
+  if (phoneNumber.toString().length !== 7) {
     return 'incorrect phone number length';
-  } else {
-    if (isNaN(Number(phoneNumber))) {
-      return 'incorrect phone number format';
-    }
+  }
+  if (isNaN(Number(phoneNumber))) {
+    return 'incorrect phone number format';
   }
   return false;
-}
+};
 
 const isInvalidName = (name) => {
   if (name.length < 3 || name.length > 255) {
     return 'incorrect name length';
   }
   const regexp = new RegExp('^[a-zA-Z]*$');
-  if (name.search(regexp) == -1) {
+  if (name.search(regexp) === -1) {
     return 'incorrect symbol in name';
   }
   return false;
-}
+};
 
 const isInvalidEmail = (email) => {
   const regexp = new RegExp('@');
@@ -48,14 +47,21 @@ const isInvalidEmail = (email) => {
     return 'no symbol @';
   }
   return false;
-}
+};
 
 const isInvalidAddress = (address) => {
   if (address.length < 5 || address.length > 255) {
     return 'incorrect address length';
   }
   return false;
-}
+};
+
+const isInvalidWishes = (wishes) => {
+  if (wishes.length > 100) {
+    return 'incorrect wishes length';
+  }
+  return false;
+};
 
 module.exports = {
   isInvalidLogin,
@@ -63,5 +69,6 @@ module.exports = {
   isInvalidPhoneNumber,
   isInvalidName,
   isInvalidEmail,
-  isInvalidAddress
-}
+  isInvalidAddress,
+  isInvalidWishes,
+};
